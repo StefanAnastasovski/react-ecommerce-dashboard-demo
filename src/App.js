d45e7576f4615d62c66b357002d864c8c810977b
Stefan Anastasovski <anastasovski.stefan96@gmail.com>
2022-07-28 16:32:52 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Vetrina Live Dashboard</h1>
+      <h2>Vetrina Live Dashboard</h2>
+      <p>Vetrina Live Dashboard</p>
+    </div>
+  );
+};
+
+export default App;
