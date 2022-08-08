296f06b40459ee2d2cea4bdd921e6a290f964e9a
Stefan Anastasovski <anastasovski.stefan96@gmail.com>
2022-08-08 18:54:25 +0200

    constants file added + TargetIcon changes

---
+++ src/data/constants.js
@@ -0,0 +1,10 @@
+export const PRIMARY_COLOR_ACTIVE = "#21B8F9";
+
+export const PRIMARY_BACKGROUND_COLOR_ACTIVE = "#F7F7F7";
+export const SECONDARY_BACKGROUND_COLOR_ACTIVE = "#E9F8FE";
+
+
+export const PRIMARY_COLOR_TEXT = "#103B66";
+export const SECONDARY_COLOR_TEXT = "#233B53";
+
+export const MENU_FONT_SIZE = "14px";
