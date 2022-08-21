59da9b4c60fbbe2b8b41c022f457ff00f6ca7294
Stefan Anastasovski <anastasovski.stefan96@gmail.com>
2022-08-21 23:06:46 +0200

    Image Comp updated

--- src/components/Image.js
+++ src/components/Image.js
@@ -2,13 +2,13 @@ import React from "react";
 
 import { CardMedia } from "@mui/material";
 
-const Image = ({ source, alt, width, ...otherProps }) => {
+const Image = ({ source, alt, width, height, ...otherProps }) => {
   return (
     <CardMedia
       component="img"
       src={source}
       title={alt}
-      sx={{ width: width || "unset" }}
+      sx={{ width: width || "unset", height: height || "unset" }}
       {...otherProps}
     />
   );
